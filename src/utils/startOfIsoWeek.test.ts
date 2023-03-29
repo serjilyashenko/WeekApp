32fbadd3399a38236e224285cae7ba77d851a610
Serge Ilyashenko <age_bookend_0j@icloud.com>
2023-03-29 19:09:02 +0200

    update startOfIsoWeek and add tests

--- src/utils/startOfIsoWeek.test.ts
+++ src/utils/startOfIsoWeek.test.ts
@@ -1,3 +1,43 @@
-it('test test', () => {
-  expect(true).toBe(true);
+import { startOfIsoWeek } from './startOfIsoWeek';
+
+it.each([
+  ['2023-01-05T15:00:00', '2023-01-02'],
+  ['2023-01-05T00:00:00', '2023-01-02'],
+  ['2023-01-05T23:59:59.999', '2023-01-02'],
+])('day edge cases: %o -> %o', (input, output) => {
+  const isoWeekFirstWeek = startOfIsoWeek(new Date(input));
+
+  expect(isoWeekFirstWeek.toLocaleDateString('en-SE')).toBe(output);
+});
+
+it.each([
+  ['2023-03-27T14:00:00', '2023-03-27'],
+  ['2023-03-28T14:00:00', '2023-03-27'],
+  ['2023-03-29T14:00:00', '2023-03-27'],
+  ['2023-03-30T14:00:00', '2023-03-27'],
+  ['2023-03-31T14:00:00', '2023-03-27'],
+  ['2023-04-01T14:00:00', '2023-03-27'],
+  ['2023-04-02T14:00:00', '2023-03-27'],
+])('weekday cases: %o -> %o', (input, output) => {
+  const isoWeekFirstWeek = startOfIsoWeek(new Date(input));
+
+  expect(isoWeekFirstWeek.toLocaleDateString('en-SE')).toBe(output);
+});
+
+it.each([
+  ['2023-01-01T15:00:00', '2022-12-26'],
+  ['2023-02-08T15:00:00', '2023-02-06'],
+  ['2023-03-18T14:00:00', '2023-03-13'],
+  ['2023-03-23T14:00:00', '2023-03-20'],
+  ['2023-03-29T14:00:00', '2023-03-27'],
+])('random tests: %o -> %o', (input, output) => {
+  const isoWeekFirstWeek = startOfIsoWeek(new Date(input));
+
+  expect(isoWeekFirstWeek.toLocaleDateString('en-SE')).toBe(output);
+});
+
+it('does not mutate the original date', () => {
+  const date = new Date('2023-03-29T14:00:00Z');
+  startOfIsoWeek(date);
+  expect(date.toISOString()).toBe('2023-03-29T14:00:00.000Z');
 });
--- src/utils/startOfIsoWeek.ts
+++ src/utils/startOfIsoWeek.ts
@@ -1,14 +1,22 @@
 const firstDayOfIsoWeek = 1; // Monday is the first day of isoWeek; https://en.wikipedia.org/wiki/ISO_week_date
 
+// 1 mon diff = 0
+// 2 tue diff = 1
+// 3 wed diff = 2
+// 4 thu diff = 3
+// 5 fri diff = 4
+// 6 sat diff = 5
+// 0 sun diff = 6
+
 export function startOfIsoWeek(dirtyDate: Date): Date {
   const date: Date = new Date(dirtyDate);
   const day: number = date.getDay();
 
   date.setHours(0, 0, 0, 0);
 
-  const diff: number = day === 0 ? 7 : day - firstDayOfIsoWeek;
+  const diff: number = day === 0 ? 6 : day - firstDayOfIsoWeek;
 
-  date.setDate(date.getDay() - diff);
+  date.setDate(date.getDate() - diff);
 
   return date;
 }
