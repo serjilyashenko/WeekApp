a4ba28b8ffcb4e49d1e43fbbd07570685ff13585
Serge Ilyashenko <age_bookend_0j@icloud.com>
2023-04-08 18:13:51 +0200

    add getIsoStringDate tests

--- src/utils/getIsoStringDate.test.ts
+++ src/utils/getIsoStringDate.test.ts
@@ -1,5 +1,20 @@
 import { getIsoStringDate } from './getIsoStringDate';
 
+it('should return the ISO string date for the given date object', () => {
+  const date = new Date('2022-04-08');
+  expect(getIsoStringDate(date)).toEqual('2022-04-08');
+});
+
+it('should add leading zeros for single-digit months and days', () => {
+  const date = new Date('2022-01-01');
+  expect(getIsoStringDate(date)).toEqual('2022-01-01');
+});
+
+it('should return the same date string for dates with a time component', () => {
+  const date = new Date('2022-04-08T15:30:00');
+  expect(getIsoStringDate(date)).toEqual('2022-04-08');
+});
+
 it('does not mutate the original date', () => {
   const date = new Date('2023-03-29T14:00:00Z');
   getIsoStringDate(date);
