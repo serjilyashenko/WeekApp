c567c6c70b16f2d373f53378b5ad0e8521ff74ff
Serge Ilyashenko <age_bookend_0j@icloud.com>
2023-07-22 22:26:49 +0200

    test getEndOfIsoWeek

--- src/utils/getEndOfIsoWeek.test.ts
+++ src/utils/getEndOfIsoWeek.test.ts
@@ -1,9 +1,11 @@
 import { getEndOfIsoWeek } from './getEndOfIsoWeek';
 
-it('test test', async () => {
-  const result = getEndOfIsoWeek(new Date());
-
-  expect(result.toLocaleString('en-SE')).toBeFalsy();
+it.each([
+  ['2023-07-22T00:00:00', '2023-07-23, 23:59:59'],
+  ['2022-12-26T15:00:00', '2023-01-01, 23:59:59'],
+  ['2023-01-03T01:00:00', '2023-01-08, 23:59:59'],
+  // TODO: Add more tests
+])('test: %o => %o', (input, output) => {
+  const date = new Date(input);
+  expect(getEndOfIsoWeek(date).toLocaleString('en-SE')).toBe(output);
 });
-
-// TODO: add more tests
--- src/utils/getEndOfIsoWeek.ts
+++ src/utils/getEndOfIsoWeek.ts
@@ -2,7 +2,7 @@ import { getIsoWeek } from './getIsoWeek';
 
 export function getEndOfIsoWeek(dirtyDate: Date): Date {
   const date = getIsoWeek(dirtyDate);
-  date.setDate(date.getDate() + 6);
+  date.setDate(date.getDate() + 7);
   date.setTime(date.getTime() - 1);
 
   return date;
