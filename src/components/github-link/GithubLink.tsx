32c33248548e3ecf361e2c9d5b0fafdf43c72b71
Serge Ilyashenko <age_bookend_0j@icloud.com>
2023-10-04 12:12:34 +0200

    fix: footer night mode and GitHub icon animation

--- src/components/github-link/GithubLink.tsx
+++ src/components/github-link/GithubLink.tsx
@@ -4,12 +4,13 @@ import style from './github-link.module.css';
 export function GithubLink(): JSX.Element {
   return (
     <a
+      className={style.link}
       href="https://github.com/serjilyashenko/WeekApp"
       target="_blank"
       rel="noreferrer"
       aria-label="github link"
     >
-      <GitHubIcon className={style.logo} />
+      <GitHubIcon aria-label="github icon" className={style.logo} />
     </a>
   );
 }
